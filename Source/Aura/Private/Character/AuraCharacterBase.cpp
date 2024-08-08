5d0dcaf415ebb014e1a980a64310b30f28a8b8fb
Kaan Kirant <kaankirant98@gmail.com>
2024-08-08 01:12:20 +0100

    Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Kaan Kirant
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
